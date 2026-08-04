7cf99f24df144a31211e0e9f7181cc8f11733c7a
Narayan Dande <narayandande100@gmail.com>
2026-08-04 14:59:45 +0530

    bean example addedd

--- spring5/src/main/java/com/college/Test.java
+++ spring5/src/main/java/com/college/Test.java
@@ -1,5 +1,17 @@
 package com.college;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
+	public static void main(String[] args) {
+		
+		ApplicationContext  context = new ClassPathXmlApplicationContext("narayan.xml");
+		((AbstractApplicationContext) context).close();
+		
+				
+	}
 
 }
