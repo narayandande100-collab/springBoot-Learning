8cae6938df906b15f107d7ce67b62a0a997e535d
Narayan Dande <narayandande100@gmail.com>
2026-08-10 23:57:46 +0530

    Initial project commit

---
+++ src/main/java/com/narayan/config.java
@@ -0,0 +1,12 @@
+package com.narayan;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class config {
+	
+	public config() {
+		System.out.println("object is created");
+	}
+
+}
